7d6d531149825c799f26c0ddaac7ec3a77f8d806
Russ Hewson <r.hewson@qmul.ac.uk>
2017-04-07 10:09:27 +0100

    Updated class constants

--- src/Random.php
+++ src/Random.php
@@ -1,10 +1,12 @@
 <?php
 namespace Meldon\Random;
 class Random {
+    const SORT_ASC = 1;
+    const SORT_DESC = 2;
     /**
      * @param $number Number of dice to roll
      * @param $sides Number of sides the dice should have
-     * @param null|'ASC'|'DESC' $order  Whether to sort the rolls and if so to sort ASC or DESC
+     * @param null|Random::SORT_ASC|Random::SORT_DESC $order  Whether to sort the rolls and if so to sort ASC or DESC
      * @return \stdClass
      */
     public static function dice( $number, $sides, $order = NULL ) {
@@ -14,9 +16,9 @@ class Random {
         for ( $i = 1; $i <= $number; $i++ ) {
             $roll->total += $roll->results[$i] = self::getInteger( 1, $sides );
         }
-        if ( $order === "ASC" ) {
+        if ( $order === Random::SORT_ASC ) {
             sort( $roll->results );
-        } elseif ( $order === "DESC" ) {
+        } elseif ( $order === Random::SORT_DESC ) {
             rsort( $roll->results );
         }
         return $roll;
